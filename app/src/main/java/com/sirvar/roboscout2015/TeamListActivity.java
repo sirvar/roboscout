84e433208040cb059e1a256b3f6c1f99d84d7e1d
Rikin Katyal <katyalrikin@gmail.com>
2016-03-13 20:37:00 -0400

    added logout

--- app/src/main/java/com/sirvar/roboscout2015/TeamListActivity.java
+++ app/src/main/java/com/sirvar/roboscout2015/TeamListActivity.java
@@ -12,6 +12,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -20,6 +22,7 @@ import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -170,6 +173,31 @@ public class TeamListActivity extends AppCompatActivity implements TeamListAdapt
                 = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_team_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            sessionManager.logout();
+            ParseUser.logOut();
+            startActivity(new Intent(getApplicationContext(), RedirectActivity.class));
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
